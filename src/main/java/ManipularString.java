b7e008e74435e44fdb9cfe93c146bd5c06cf89e6
Elton Santos <eltonaxl@hotmail.com>
2017-10-11 13:41:10 -0300

    Commit more things

--- src/main/java/ManipularString.java
+++ src/main/java/ManipularString.java
@@ -1,23 +1,39 @@
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "manipularString")
 public class ManipularString {
     
     private String texto;
+    private String tratado;
     
     public String tratarTexto(){
-    
-        return texto;
+        
+        ArrayList<String> palavrasChaves = new ArrayList<>();
+        palavrasChaves.add("PROTOCOLO");
+        palavrasChaves.add("ÓRGÃO");
+        palavrasChaves.add("DATA DE REGISTRO");
+        palavrasChaves.add("NOME MANIFESTANTE");
+        palavrasChaves.add("E-MAIL MANIFESTANTE");
+        palavrasChaves.add("ENDEREÇO MANIFESTANTE");
+        palavrasChaves.add("TELEFONE MANIFESTANTE");
+        palavrasChaves.add("ASSUNTO");
+        palavrasChaves.add("TEXTO DA MANIFESTAÇÃO");
+        palavrasChaves.add("CONTEXTUALIZAÇÃO PARA ÁREA COMPETENTE");
+        
+        System.out.println("------------------------------");
+        System.out.println("Lista: " +palavrasChaves);
+        
+        tratado = texto.replaceAll(" ", "");
+        System.out.println(tratado);
+        return tratado;    
     }
 
-    
     public ManipularString() {
-        System.out.println("Texto sendo manipulado...");
-        
+        /*System.out.println("Texto sendo manipulado...");
         String Str = new String("Welcome to Tutorialspoint.com");
-
         System.out.print("Return Value :" );
-        System.out.println(Str.substring(10) );
+        System.out.println(Str.substring(10) );*/
     }
 	  
     public String getTexto() {
@@ -28,6 +44,12 @@ public class ManipularString {
         this.texto = texto;
     }
     
-    
+    public String getTratado() {
+        return tratado;
+    }
+
+    public void setTratado(String tratado) {
+        this.texto = tratado;
+    }
    
 }
